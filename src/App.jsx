8ebe85ca58b7cf00924a289c2d67126e392bca6b
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-02 19:21:05 +0530

    Add pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AllMeetUpsPage from "./pages/AllMeetups";
+import FavouritesPage from "./pages/Favourites";
+import NewMeetupPage from "./pages/NewMeetup";
+const routing = createBrowserRouter([
+  { path: "/", element: <AllMeetUpsPage /> },
+  { path: "/new-meetup", element: <NewMeetupPage /> },
+  { path: "/favourites", element: <FavouritesPage /> },
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={routing} />;
 }
 
 export default App;
