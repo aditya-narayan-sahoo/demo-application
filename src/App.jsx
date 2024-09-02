a1ada9e311d0cdada2462f1980a6a9484ec9b327
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-02 20:30:46 +0530

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AllMeetUpsPage from "./pages/AllMeetups";
 import FavouritesPage from "./pages/Favourites";
 import NewMeetupPage from "./pages/NewMeetup";
-const routing = createBrowserRouter([
-  { path: "/", element: <AllMeetUpsPage /> },
-  { path: "/new-meetup", element: <NewMeetupPage /> },
-  { path: "/favourites", element: <FavouritesPage /> },
-]);
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <RouterProvider router={routing} />;
+  return (
+    <BrowserRouter>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetUpsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
